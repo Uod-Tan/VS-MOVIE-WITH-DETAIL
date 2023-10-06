44737e872bfdf8a76300316be3e58c5a3295e8ca
Dave, Elijah R <145178111+Uod-Tan@users.noreply.github.com>
2023-10-06 20:29:55 +0800

    Add files via upload

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ASD.Models;
+namespace ASD.Controllers
+{
+    public class MovieController : Controller
+    {
+        public ViewResult Random()
+        {
+            var movie = GetMovie();
+                return View(movie);
+        }
+        public ActionResult Details(int id) 
+        {
+            var movie =GetMovie().SingleOrDefault(c=> c.Id == id);
+            if (movie == null)
+                return View();
+            return View(movie); 
+        }
+        private IEnumerable<Movie> GetMovie() 
+        {
+            return new List<Movie>
+            { new Movie{Id= 1, Name = "mov1" },
+            new Movie {Id= 2, Name= "mov2" }
+            };
+        }
+    }
+}
